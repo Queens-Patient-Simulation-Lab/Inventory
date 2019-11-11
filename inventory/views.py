177746bd830ff59f63f6ee50de89f447ceadf12d
Justin Gerhardt <justin@gerhardt.link>
2019-11-11 02:44:10 -0500

    Create demo report

--- inventory/views.py
+++ inventory/views.py
@@ -12,5 +12,9 @@ def login(request):
 def homePage(request):
     return render(request, 'inventory/homepage.html')
 
+def sampleReport(request):
+    return render(request, 'inventory/report_sample.html')
+
+
 def sampleItem(request):
     return render(request, 'inventory/sample-item.html')
