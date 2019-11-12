6d7b34dcc30d66473e58af2840ee660d6ed84ef1
liamwalsh <liam.walsh@queensu.ca>
2019-11-12 13:39:52 -0500

    Made homepage mockup and added some more photos.
    Tagged all views with @xframe_options_exempt to allow embedding of the web page in presentation

--- inventory/views.py
+++ inventory/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 # Create your views here.
+@xframe_options_exempt
 def login(request):
     print(request.method)
     if (request.method == 'POST'):
-        return redirect('inventory-homepage')
+        return redirect('homepage')
     else:
         return render(request, 'inventory/login.html')
 
+@xframe_options_exempt
 def homePage(request):
     return render(request, 'inventory/homepage.html')
 
+@xframe_options_exempt
 def sampleReport(request):
     return render(request, 'inventory/report_sample.html')
 
 
+@xframe_options_exempt
 def sampleItem(request):
     return render(request, 'inventory/sample-item.html')
