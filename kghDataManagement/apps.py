be177a43938394e5d634d43602ed63526091e25a
zachary Freedman <zacharyfreedman@zacharys-MacBook-Pro.local>
2019-11-24 00:13:36 -0500

    Created all components as apps

---
+++ kghDataManagement/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KghdatamanagementConfig(AppConfig):
+    name = 'kghDataManagement'
--- simulation_lab/settings.py
+++ simulation_lab/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'inventory.apps.InventoryConfig',
+    'itemManagement.apps.InventoryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
