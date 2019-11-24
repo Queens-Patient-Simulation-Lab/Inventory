04b6dfa342abc60e41381a0437c70ec9c3c60403
zachary Freedman <zacharyfreedman@zacharys-MacBook-Pro.local>
2019-11-24 01:23:16 -0500

    homepage now shows whatever items we pass to it

--- itemManagement/views.py
+++ itemManagement/views.py
@@ -7,10 +7,29 @@ from django.views.decorators.clickjacking import xframe_options_exempt
 
 @xframe_options_exempt
 def homePage(request):
-    return render(request, 'itemManagement/homepage.html')
+    itemOne = {
+        'images': ['itemManagement/nacl.jpg'],
+        'name': "NaCl 0.9% 500ml",
+        'locations': ['Main Storage Area'],
+        'totalQuantity': 15
+    }
+    itemTwo = {
+        'images': ['itemManagement/puppy.jpeg'],
+        'name': "Puppy",
+        'locations': ['Kennel'],
+        'totalQuantity': 385
+    }
+    itemThree = {
+        'images': ['itemManagement/puppy2.jpeg'],
+        'name': "Puppy 2",
+        'locations': ['Kennel'],
+        'totalQuantity': 50
+    }
+    context = {
+        'items': [itemOne, itemTwo, itemThree]
+    }
 
-
-
+    return render(request, 'itemManagement/homepage.html', context=context)
 
 @xframe_options_exempt
 def sampleItem(request):
