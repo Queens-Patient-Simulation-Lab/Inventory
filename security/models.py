ad3b96fbf4d2e06931d4916bae317ace6c79239a
zachary Freedman <zacharyfreedman@zacharys-MacBook-Pro.local>
2019-12-06 21:09:35 -0500

    Created a Custom User class and system not requires login to access

--- security/models.py
+++ security/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.db import models
 
+
 # Create your models here.
+# Resources on normal user models: https://docs.djangoproject.com/en/2.2/ref/contrib/auth/
+# Resources for creating a custom user model
+#   https://docs.djangoproject.com/en/2.2/topics/auth/customizing/#a-full-example
+#   https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html
+
+# Creating our own version of auth/user model since we authenticate by email, not username
+
+class UserManager(BaseUserManager):
+    # Specify mandatory fields as parameters and optional fields can fit in **extra_fields
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields['is_superuser'] = True
+        return self._create_user(email, password, **extra_fields)
+# The fields (password, is_active, username) are included in AbstractBaseUser
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        unique=True
+    )
+    objects = UserManager()
+    completedTutorial = models.BooleanField(default=False)
+    receivesAlerts = models.BooleanField(default=False)  # Todo, would this be better as its own table relation?
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['']
--- security/views.py
+++ security/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.clickjacking import xframe_options_exempt
-
+from django.contrib import auth
 
 # Create your views here.
+from global_login_required import login_not_required
+
+from security.models import User
 
 
 @xframe_options_exempt
+@login_not_required
 def login(request):
-    print(request.method)
-    if (request.method == 'POST'):
+    if (request.user.is_authenticated):
         return redirect('homepage')
+
+    # just for now, we create a default user if it doesn't exist
+    user = User.objects.filter(email="test@test.com").first()
+    if not user:
+        print("Making new user")
+        user = User.objects.create_user("test@test.com", "abcd")
+        assert user.password != "abcd"
+
+    if (request.method == 'POST'):
+        email = request.POST.get('email', '')
+        password = request.POST.get('password', "")
+        # todo: field validation
+        user = auth.authenticate(request, username=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('homepage')
+        else:
+            return redirect('login')
     else:
         return render(request, 'security/login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
