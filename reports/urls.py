f271f8022303bf366a194e5693da14634dd4d8c6
Justin Gerhardt <justin@gerhardt.link>
2020-01-21 04:28:36 -0500

    Add basic CSV support to reports

--- reports/urls.py
+++ reports/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.MainPage, name="reports"),
-    path('OnHand', views.InventoryOnHand, name="inventory-on-hand"),
-    path('cycle', views.CycleCount, name="cycle-count")
+    re_path(r'^onHand/(?:(?P<format>(?:csv|pdf))/)?$', views.InventoryOnHand, name="inventory-on-hand"),
+    re_path(r'^cycle/(?:(?P<format>(?:csv|pdf))/)?$', views.CycleCount, name="cycle-count")
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,20 +1,40 @@
 from django.shortcuts import render
 from itemManagement.models import Item
+import csv
+from django.http import HttpResponse
+from datetime import date
 
 def MainPage(request):
     return render(request, 'main.html')
 
+def __CSVResponseGenerator(filename, header, rows):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = f'attachment; filename="{filename}.csv"'
+    writer = csv.writer(response)
+    writer.writerow(header)
+    writer.writerows(rows)
+    return response
 
-def CycleCount(request):
+def __GetFormattedDate():
+    return date.today().strftime("%Y-%m-%d")
+
+def CycleCount(request, format=None):
     data = []
-    for item in Item.objects.all():
-        item = item.getItemDetails()
-        locations = [{"location": x["name"], "quantity": x["quantity"]} for x in item["locations"]]
-        data.append({"name": item["name"], "locations": locations})
-    data = sorted(data, key = lambda x: x["name"])
-    return render(request, 'cycle.html', {"items": data})
+    items = map(lambda x: x.getItemDetails(),Item.objects.all().order_by("title"))
+    if format == "csv":
+        for item in items:
+            for location in item["locations"]:
+                data.append([item["name"], location["name"], location["quantity"]])
+        return __CSVResponseGenerator("CycleCount-" + __GetFormattedDate(), ["Name", "Location", "Quantity"], data)
+    else:
+        for item in items:
+            locations = [{"location": x["name"], "quantity": x["quantity"]} for x in item["locations"]]
+            data.append({"name": item["name"], "locations": locations})
+        return render(request, 'cycle.html', {"items": data})
 
-def InventoryOnHand(request):
-    data = [{"name": x.title, "total": x.totalQuantity} for x in Item.objects.all()]
-    data = sorted(data, key = lambda x: x["name"])
-    return render(request, 'onHand.html', {"items": data})
+def InventoryOnHand(request, format=None):
+    data = [{"name": x.title, "total": x.totalQuantity} for x in Item.objects.all().order_by("title")]
+    if format == "csv":
+        return __CSVResponseGenerator("Inventory-on-Hand-" + __GetFormattedDate(), ["Name", "Count"], [[x["name"], x["total"]] for x in data]) 
+    else:
+        return render(request, 'onHand.html', {"items": data})
