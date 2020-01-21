342446890e38a5b1d28631b16ed61fd89cf702f2
Justin Gerhardt <justin@gerhardt.link>
2020-01-21 03:31:24 -0500

    Remove Sample Report

--- reports/views.py
+++ reports/views.py
@@ -1,8 +1,1 @@
 from django.shortcuts import render
-from django.views.decorators.clickjacking import xframe_options_exempt
-
-
-
-@xframe_options_exempt
-def sampleReport(request):
-    return render(request, 'reports/report_sample.html')
