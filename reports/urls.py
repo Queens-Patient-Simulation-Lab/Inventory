975e232a6239f5274711bf09b00bb5c5577a1532
Justin Gerhardt <justin@gerhardt.link>
2020-01-21 03:32:37 -0500

    Create Inventory on Hand Report

--- reports/urls.py
+++ reports/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('OnHand', views.InventoryOnHand, name="inventory-on-hand")
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,1 +1,8 @@
 from django.shortcuts import render
+from itemManagement.models import Item
+
+
+def InventoryOnHand(request):
+    data = [{"name": x.title, "total": x.totalQuantity} for x in Item.objects.all()]
+    data = sorted(data, key = lambda x: x["name"])
+    return render(request, 'onHand.html', {"items": data})
