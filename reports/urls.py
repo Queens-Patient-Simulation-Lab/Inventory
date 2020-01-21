a4d9adf492b9498b7ead1e1c575f5f98df84c398
Justin Gerhardt <justin@gerhardt.link>
2020-01-21 03:33:29 -0500

    Create Cycle Count Report

--- reports/urls.py
+++ reports/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('OnHand', views.InventoryOnHand, name="inventory-on-hand")
+    path('OnHand', views.InventoryOnHand, name="inventory-on-hand"),
+    path('cycle', views.CycleCount, name="cycle-count")
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from itemManagement.models import Item
 
+def CycleCount(request):
+    data = []
+    for item in Item.objects.all():
+        item = item.getItemDetails()
+        locations = [{"location": x["name"], "quantity": x["quantity"]} for x in item["locations"]]
+        data.append({"name": item["name"], "locations": locations})
+    data = sorted(data, key = lambda x: x["name"])
+    return render(request, 'cycle.html', {"items": data})
 
 def InventoryOnHand(request):
     data = [{"name": x.title, "total": x.totalQuantity} for x in Item.objects.all()]
