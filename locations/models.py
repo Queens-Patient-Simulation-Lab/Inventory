633fd360c22dd8d2dbc69249b1cc9ffe8e6394e7
liamwalsh <liam.walsh@queensu.ca>
2020-01-27 11:22:58 -0500

    added initial model and database

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    # address = models.CharField
+    # roomNumber = models.CharField
+
+
+def __str__(self):
+    return self.name
