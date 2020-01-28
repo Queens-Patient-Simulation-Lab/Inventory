9d24ca09ed9dd8e56dda68d561af7c9401e51973
zachary Freedman <zacharyfreedman@zacharys-MacBook-Pro.local>
2020-01-28 17:48:57 -0500

    You can now add and safe delete locations

--- itemManagement/urls.py
+++ itemManagement/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+
+from itemManagement.views import LocationView
 from . import views
 
 urlpatterns = [
     path('item/<int:itemId>', views.sampleItem, name="item-details"),
     path('home/', views.homePage, name="homepage"),
-    path('locations/', views.locationList, name="location-list")
+    path('locations/', LocationView.as_view(), name="location-list"),
+    path('locations/<int:id>/', LocationView.as_view(), name="location-list")
 ]
