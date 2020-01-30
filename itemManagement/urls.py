d4293b0e9d4e45b54d7842f0bfc7b24d3fd67783
zachary Freedman <zacharyfreedman@zacharys-MacBook-Pro.local>
2020-01-30 17:03:36 -0500

    Finished testing Location and removed method stubs for selenium testing

--- itemManagement/urls.py
+++ itemManagement/urls.py
@@ -3,9 +3,16 @@ from django.urls import path
 from itemManagement.views import LocationView
 from . import views
 
+URL_LOCATION_LIST = "location-list"
+
+URL_HOMEPAGE = "homepage"
+
+URL_ITEM_DETAILS = "item-details"
+
+
 urlpatterns = [
-    path('item/<int:itemId>', views.sampleItem, name="item-details"),
-    path('home/', views.homePage, name="homepage"),
-    path('locations/', LocationView.as_view(), name="location-list"),
-    path('locations/<int:id>/', LocationView.as_view(), name="location-list")
+    path('item/<int:itemId>', views.sampleItem, name=URL_ITEM_DETAILS),
+    path('home/', views.homePage, name=("%s" % URL_HOMEPAGE)),
+    path('locations/', LocationView.as_view(), name=("%s" % URL_LOCATION_LIST)),
+    path('locations/<int:id>/', LocationView.as_view(), name=URL_LOCATION_LIST)
 ]
