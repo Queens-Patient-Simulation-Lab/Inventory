fda92cef3dd0f56173a01b68d917b39504b8257c
Justin Gerhardt <justin@gerhardt.link>
2020-02-04 01:56:32 -0500

    Add pdf reports

--- reports/urls.py
+++ reports/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.MainPage, name="reports"),
     re_path(r'^onHand/(?:(?P<format>(?:csv|pdf))/)?$', views.InventoryOnHand, name="inventory-on-hand"),
-    re_path(r'^cycle/(?:(?P<format>(?:csv|pdf))/)?$', views.CycleCount, name="cycle-count")
+    path('cycle/', views.CycleCountHTML, name="cycle-count"),
+    path('cycle/csv', views.CycleCountCSV, name="cycle-count-csv"),
+    path('cycle/pdf', views.CycleCountPDF, name="cycle-count-pdf")
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,40 +1,67 @@
-from django.shortcuts import render
-from itemManagement.models import Item
 import csv
-from django.http import HttpResponse
+import io
+from django.shortcuts import render
+from django.template.loader import render_to_string
+from itemManagement.models import Item
+from django.http import HttpResponse, FileResponse
 from datetime import date
+from weasyprint import HTML
+
 
 def MainPage(request):
-    return render(request, 'main.html')
+    return render(request, 'web/main.html')
 
-def __CSVResponseGenerator(filename, header, rows):
+
+def __CSVResponseGenerator(filenamePrefix, header, rows):
     response = HttpResponse(content_type='text/csv')
-    response['Content-Disposition'] = f'attachment; filename="{filename}.csv"'
+    response['Content-Disposition'] = f'attachment; filename="{filenamePrefix}-{__GetFormattedDate()}.csv"'
     writer = csv.writer(response)
     writer.writerow(header)
     writer.writerows(rows)
     return response
 
+
+def __PDFResponseGenerator(filenamePrefix, title, template, args):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="{filenamePrefix}-{__GetFormattedDate()}.pdf"'
+    args["title"] = title
+    content = render_to_string(template, args)
+    HTML(string=content).write_pdf(response)
+    return response
+
+
 def __GetFormattedDate():
     return date.today().strftime("%Y-%m-%d")
 
-def CycleCount(request, format=None):
+def CycleCountHTML(request):
     data = []
-    items = map(lambda x: x.getItemDetails(),Item.objects.all().order_by("title"))
-    if format == "csv":
-        for item in items:
-            for location in item["locations"]:
-                data.append([item["name"], location["name"], location["quantity"]])
-        return __CSVResponseGenerator("CycleCount-" + __GetFormattedDate(), ["Name", "Location", "Quantity"], data)
-    else:
-        for item in items:
-            locations = [{"location": x["name"], "quantity": x["quantity"]} for x in item["locations"]]
-            data.append({"name": item["name"], "locations": locations})
-        return render(request, 'cycle.html', {"items": data})
+    items = map(lambda x: x.getItemDetails(), Item.objects.all().order_by("title"))
+    for item in items:
+        locations = [{"location": x["name"], "quantity": x["quantity"]} for x in item["locations"]]
+        data.append({"name": item["name"], "locations": locations})
+    return render(request, 'web/cycle.html', {"items": data})
+
+def CycleCountCSV(request):
+    data = []
+    items = map(lambda x: x.getItemDetails(), Item.objects.all().order_by("title"))
+    for item in items:
+        for location in item["locations"]:
+            data.append([item["name"], location["name"], location["quantity"]])
+    return __CSVResponseGenerator("CycleCount", ["Name", "Location", "Quantity"], data)
+
+def CycleCountPDF(request):
+    data = []
+    items = map(lambda x: x.getItemDetails(), Item.objects.all().order_by("title"))
+    for item in items:
+        locations = [{"location": x["name"], "quantity": x["quantity"]} for x in item["locations"]]
+        data.append({"name": item["name"], "locations": locations})
+    return __PDFResponseGenerator("CycleCount", "Cycle Count", 'pdf/cycle.html', {"items": data})
 
 def InventoryOnHand(request, format=None):
     data = [{"name": x.title, "total": x.totalQuantity} for x in Item.objects.all().order_by("title")]
     if format == "csv":
-        return __CSVResponseGenerator("Inventory-on-Hand-" + __GetFormattedDate(), ["Name", "Count"], [[x["name"], x["total"]] for x in data]) 
+        return __CSVResponseGenerator("Inventory-on-Hand", ["Name", "Count"], [[x["name"], x["total"]] for x in data])
+    elif format == "pdf":
+        return __PDFResponseGenerator("Inventory-on-Hand", "Inventory on Hand", 'pdf/onHand.html', {"items": data})
     else:
-        return render(request, 'onHand.html', {"items": data})
+        return render(request, 'web/onHand.html', {"items": data})
