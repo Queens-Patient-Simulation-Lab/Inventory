c831dfb71a218e27c5e5cb85a37f3ff185c43ecb
liamwalsh <liam.walsh@queensu.ca>
2020-03-25 20:31:00 -0400

    removed document.ready wrapper in jquery for modal javascript

--- assets/js/item_details.js
+++ assets/js/item_details.js
@@ -3,69 +3,66 @@ import 'bootstrap4-tagsinput/tagsinput'
     Called when the plus button next to a quantity is pressed
  */
 
-$(document).ready(function() {
+//--------------TAGS-----------------
 
-    //--------------TAGS-----------------
+$(".tagsInputBoxContainer").hide();
 
-    $(".tagsInputBoxContainer").hide();
-
-    $.each($(".tagsShown").children().map(function(){return $(this).text();}).get(), function(index, value) {
-        $('#tagsInputBox').tagsinput('add', value);
-    });
-
-    $("#tagsInputBox").tagsinput({
-      trimValue: true
-    });
-
-    $(".tagEditBtn").click(function () {
-        $(".tagsInputBoxContainer").show();
-        $(".tagsShown").hide();
-        $(".tagEditBtn").hide();
-    });
-
-    //--------------------------------
-
-    $(".increment").click(function () {
-        addQuantity(1, $(this))
-    });
-    /*
-        Called when the minus button next to a quantity is pressed
-     */
-    $(".decrement").click(function () {
-            console.log("CLICKED decrement");
-            //if quantity is less than zero, do not decrement further
-            if (parseInt($(this).siblings(".item_quantity").val()) <= 0) {
-                console.log("Could not decrement further")
-            }
-            else{
-                addQuantity(-1, $(this))
-            }
-    });
-
-    function addQuantity(value, caller) {
-        caller.siblings(".item_quantity").val(
-            parseInt(caller.siblings(".item_quantity").val()) + value
-        );
-    }
-
-
-    /* Async submit request */
-    $("#item-details-form").submit(function(e) {
-
-        e.preventDefault(); // avoid to execute the actual submit of the form.
-
-        var form = $(this);
-        var url = form.attr('action');
-
-        $.ajax({
-               type: "POST",
-               url: url,
-               data: form.serialize(), // serializes the form's elements.
-               success: function(data)
-               {
-                   $(".modal").modal("hide");
-               }
-             });
-        });
-
+$.each($(".tagsShown").children().map(function(){return $(this).text();}).get(), function(index, value) {
+    $('#tagsInputBox').tagsinput('add', value);
 });
+
+$("#tagsInputBox").tagsinput({
+  trimValue: true
+});
+
+$(".tagEditBtn").click(function () {
+    $(".tagsInputBoxContainer").show();
+    $(".tagsShown").hide();
+    $(".tagEditBtn").hide();
+});
+
+//--------------------------------
+
+$(".increment").click(function () {
+    addQuantity(1, $(this))
+});
+/*
+    Called when the minus button next to a quantity is pressed
+ */
+$(".decrement").click(function () {
+        console.log("CLICKED decrement");
+        //if quantity is less than zero, do not decrement further
+        if (parseInt($(this).siblings(".item_quantity").val()) <= 0) {
+            console.log("Could not decrement further")
+        }
+        else{
+            addQuantity(-1, $(this))
+        }
+});
+
+function addQuantity(value, caller) {
+    caller.siblings(".item_quantity").val(
+        parseInt(caller.siblings(".item_quantity").val()) + value
+    );
+}
+
+
+/* Async submit request */
+$("#item-details-form").submit(function(e) {
+
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+
+    var form = $(this);
+    var url = form.attr('action');
+
+    $.ajax({
+           type: "POST",
+           url: url,
+           data: form.serialize(), // serializes the form's elements.
+           success: function(data)
+           {
+               $(".modal").modal("hide");
+           }
+         });
+    });
+
