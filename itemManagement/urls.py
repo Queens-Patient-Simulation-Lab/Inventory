544c482cac940c7baf4b3e16ed638ccd3cb1c233
PinkViolet <44185081+PinkViolet@users.noreply.github.com>
2020-04-06 20:29:24 -0400

    Item delete

--- itemManagement/urls.py
+++ itemManagement/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path
 
-from itemManagement.views import LocationView, ItemDetailsView, HomePage
+from itemManagement.views import LocationView, ItemDetailsView, HomePage, ItemDeleteView
 from . import views
 
 URL_LOCATION_LIST = "location-list"
@@ -11,8 +11,11 @@ URL_ITEM_DETAILS = "item-details"
 
 URL_GET_PHOTO = "get-photo"
 
+URL_ITEM_DELETE = "item-delete"
+
 urlpatterns = [
     re_path(r'^item/(?P<itemId>.*)?$', ItemDetailsView.as_view(), name=URL_ITEM_DETAILS),
+    path('item_delete/<int:itemId>', ItemDeleteView.as_view(), name=URL_ITEM_DELETE),
     path('home/', HomePage.as_view(), name=URL_HOMEPAGE),
     path('locations/', LocationView.as_view(), name=URL_LOCATION_LIST),
     path('locations/<int:id>/', LocationView.as_view(), name=URL_LOCATION_LIST),
