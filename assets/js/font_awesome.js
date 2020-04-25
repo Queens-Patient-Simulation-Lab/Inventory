4bf2f0a4e95332bd03e55be71ee41ea940eef64e
Justin Gerhardt <justin@gerhardt.link>
2020-04-25 12:16:33 -0400

    Make image delete button an icon

--- assets/js/font_awesome.js
+++ assets/js/font_awesome.js
@@ -2,8 +2,8 @@ import { library, dom, config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false // needed for CSP
 
 // import any used icons here
-import { faCamera } from "@fortawesome/free-solid-svg-icons";
-library.add(faCamera);
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+library.add(faTrashAlt);
 
 
 dom.watch();
